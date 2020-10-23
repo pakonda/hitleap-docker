7dd49d4682e2d45b7db54304311bf8be84323593
pakonda <pakondaman@gmail.com>
2020-10-23 15:05:14 +0700

    add warning message

--- hitleap_autoshut.sh
+++ hitleap_autoshut.sh
@@ -5,6 +5,8 @@ secs=$(($1 * 60))
 if [ $secs -ne 0 ]; then
     echo "HitLeap will terminate in $1 minutes"
     sleep $secs
+    echo "HitLeap will terminate in 10 sec"
+    sleep 10
     pkill -f HitLeap-Viewer
 fi
 
